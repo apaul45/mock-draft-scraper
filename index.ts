791bff334b3a5da05169b6fb39085e62f19490d9
Ayon Paul <ayon.paul@stonybrook.edu>
2023-12-17 23:14:36 -0500

    Add MFD scraper

--- index.ts
+++ index.ts
@@ -1,7 +1,89 @@
 import puppeteer from "puppeteer";
 import { load } from "cheerio";
 import { readFileSync, writeFileSync, writeFile } from "fs";
-import { Player } from "./utils";
+import { Player, Positions } from "./utils";
+
+async function scrapeMFD(teamsList: { [key: string]: string }) {
+  const draft: Player[] = [];
+
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  page.setDefaultNavigationTimeout(0); //Ads dynamically load in, so need sufficient (infinite) timeout for loading to finish
+
+  await page.goto("https://www.nflmockdraftdatabase.com/mock-draft-simulator");
+
+  await page.waitForSelector("li[value='1']");
+  await page.click("li[value='1']");
+  await page.click("li[value='250']");
+
+  await page.click(".hoverable.br-4px.w100p.mt-20px.brd-none.pd-20px.bold.bg-primary.color-white");
+
+  await page.waitForSelector(".five-column-list.list-style-type-none.pd-0.mb-4px");
+  await page.waitForSelector(".pdtb10-lr2.brd-none.w100p.bold.fs-14px.bg-none.color-white > .fas.fa-sync.fs-24px");
+  //await page.click(".pdtb10-lr2.brd-none.w100p.bold.fs-14px.bg-none.color-white > .fas.fa-sync.fs-24px");
+
+  await page.$eval(".fas.fa-sync.fs-24px", (el) => {
+    //@ts-ignore
+    el.click();
+  });
+
+  //@ts-ignore
+  await page.$eval(".pdtb10-lr2.brd-none.w100p.bold.fs-14px.bg-none.color-orange", (el) => el.click());
+
+  await page.waitForSelector(".modal-content", { timeout: 90000 });
+  await page.click(".hoverable.pdtb10-lr2.w100p.bold.fs-14px.bg-white.color-sec.brd-1px-sec.btn.btn-primary");
+  await page.click(".hoverable.pdtb10-lr2.w100p.bold.fs-14px.bg-white.color-sec.brd-1px-sec.btn.btn-primary");
+
+  await page.waitForSelector(".mock-list");
+
+  const html = load(await page.content());
+
+  html(".mock-list:first").remove(); // Remove selections made for selected team to simplify querying
+
+  const playerNames = html(".mock-list-item")
+    .find(".player-name.player-name-bold > a")
+    .toArray()
+    // @ts-ignore
+    .map((element) => element.children[0].data);
+
+  const positions = html(".mock-list-item")
+    .find(".player-details")
+    .toArray()
+    .map((element) => {
+      // @ts-ignore
+      const text: string = element.children[0].data;
+      return text.substring(0, text.indexOf(" "));
+    });
+
+  const schools = html(".mock-list-item")
+    .find(".player-details > a")
+    .toArray()
+    //@ts-ignore
+    .map((element) => element.children[1].data);
+
+  // Need to transform teamNames to their associated abbreviation
+  const teams = html(".mock-list-item")
+    .find(".team-link > img")
+    .toArray()
+    .map((element) => {
+      const alt = element.attribs["alt"];
+      return teamsList[alt.substring(0, alt.indexOf(" "))];
+    });
+
+  teams.forEach((team, index) => {
+    const player: Player = {
+      name: playerNames[index],
+      team: team,
+      position: positions[index] as unknown as Positions,
+      school: schools[index],
+    };
+
+    draft.push(player);
+  });
+
+  writeFileSync("./sites/MFD.json", JSON.stringify(draft));
+  await browser.close();
+}
 
 async function scrapePFN() {
   const draft: Player[] = [];
@@ -83,8 +165,22 @@ function formatAndWriteData() {
 }
 
 async function main() {
+  let data = readFileSync("./utils/teams.json", { encoding: "utf8", flag: "r" });
+  const teamsList: { [key: string]: any } = JSON.parse(data);
+
+  // Needed for MFD scraping
+  const reverseTeamsList = Object.fromEntries(
+    Object.entries(teamsList).map(([key, value]) => {
+      const fullName = value["fullName"];
+
+      return [fullName.substring(fullName.lastIndexOf(" ") + 1), key];
+    })
+  );
+
+  await scrapeMFD(reverseTeamsList);
+
   // await scrapePFN();
-  formatAndWriteData();
+  //formatAndWriteData();
 }
 
 main();
